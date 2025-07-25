6fcaa7b809af66af1e1c3a839f204fceaf21a362
Gowtham Shankar Krishnamoorthy <124244143+gowthamshankar05@users.noreply.github.com>
2025-07-25 18:41:23 +0530

    k8s cluster setup
    * k8s cluster setup
    
    * k8s cluster setup
    
    * k8s cluster setup

--- tf-resources/ec2.tf
+++ tf-resources/ec2.tf
@@ -1,13 +1,13 @@
 
 
-resource "aws_key_pair" "deployer" {
+resource "aws_key_pair" "ssh_key" {
   key_name   = "coimbatore-cluster-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCk32HB+6NhU2+ZRbbxcw6wEUoCY0eFIu7jTuz5cYp6AOJJjJg7HejaJJCvNpj/TMEGaodqhbrYhTJgGtNGLlE3EIKvAoExwK6SNEXV5bn9x5gJfa33omEkZX/CeJJGY0qBPXwDSIHQeUDdNAWsa+cWsc4R/v1agoVUNkWZOvWysGrVrZgKQVlIkME4tTym90Rt8TYk0aPGTzaxpMyQ7l2ZmO5/cYHRiUyabaylRg7C5cs+axVwSvOxc/pNdZcTfU4ryWq/LQ880+x4jM0d6PMiAFKtppr+Cun87o/rkAn8PBD+ef2cRj5AfrBtsKVvqtk65c31+33etaNyaGF35C0igaGrNdGVrJWpcO1XQ1qLUFzR5O+UTyHiR7NrwHLx7E8ubmKJoWz10CgctIC6SaJF84JUM8fbVVR4s5Gat/1/KhpwyW+JZherSu+O/C/46NWcb1LhycpIwM2HDyNrDsEnEzpnTmwrFdxJOhCNjcpg7CvsZSSznDbxber2btJimNHHlPS4bXGIF3/4WSYDDLKISRle+RmDc6pRicn10r6Nx62qjDa6xI1YYnh4PXZ5ZjS1hlzJDtXybNVR1dZPTE3xUM0p3Dlt3y6SIrwpgSTCRWowpJ8sksPTXllbB6awO2Z2KAveMHlKikMYRrqTGk0aFO+CbWm1M8eSjAHSGjpJ3Q== gowthamshankarkrishnamoorthy@Gowthams-MacBook-Pro.local"
 }
 
 data "aws_ami" "cluster-ami" {
   most_recent = true
-  owners      = ["099720109477"]   # Canonical (Ubuntu)
+  owners      = ["099720109477"] # Canonical (Ubuntu)
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
@@ -18,12 +18,28 @@ data "aws_ami" "cluster-ami" {
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.cluster-ami.id
   instance_type               = "t2.medium"
-  key_name                    = aws_key_pair.deployer.key_name
+  key_name                    = aws_key_pair.ssh_key.key_name
   subnet_id                   = aws_subnet.cluster-public-subnets[0].id
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.control-plane-sg.id]
+  vpc_security_group_ids      = [aws_security_group.bastion-sg.id]
 
   tags = {
     Name = "${var.cluster_name}-bastion"
   }
 }
+
+resource "aws_instance" "control_plane" {
+  ami                         = data.aws_ami.cluster-ami.id
+  instance_type               = "t2.medium"
+  key_name                    = aws_key_pair.ssh_key.key_name
+  subnet_id                   = aws_subnet.cluster-private-subnets[0].id
+  vpc_security_group_ids      = [aws_security_group.control-plane-sg.id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "${var.cluster_name}-control-plane"
+  }
+
+  user_data = file("${path.module}/scripts/bootstrap.sh")
+}
+
--- tf-resources/securitygroup.tf
+++ tf-resources/securitygroup.tf
@@ -7,22 +7,21 @@ resource "aws_security_group" "bastion-sg" {
   tags = {
     Name = "${var.cluster_name}-bastion-sg"
   }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_b" {
-  security_group_id = aws_security_group.bastion-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
-}
+  ingress {
+    cidr_blocks = [aws_vpc.cluster-vpc.cidr_block]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
 
-resource "aws_vpc_security_group_egress_rule" "all_traffic_b" {
-  security_group_id = aws_security_group.bastion-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
 }
-
 
 resource "aws_security_group" "control-plane-sg" {
   name        = "${var.cluster_name}-control-plane-sg"
@@ -32,46 +31,43 @@ resource "aws_security_group" "control-plane-sg" {
   tags = {
     Name = "${var.cluster_name}-control-plane-sg"
   }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_cp" {
-  security_group_id = aws_security_group.control-plane-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+  ingress {
+    cidr_blocks = [aws_instance.bastion.public_ip]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "control_plane_components" {
-  security_group_id = aws_security_group.control-plane-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 10250
-  to_port           = 10259
-  ip_protocol       = "tcp"
-}
+  ingress {
+    cidr_blocks = [aws_vpc.cluster-vpc.cidr_block]
+    from_port   = 10250
+    to_port     = 10259
+    protocol    = "tcp"
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "kubernetes_api" {
-  security_group_id = aws_security_group.control-plane-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 6443
-  to_port           = 6443
-  ip_protocol       = "tcp"
-}
+  ingress {
+    cidr_blocks = [aws_vpc.cluster-vpc.cidr_block]
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "etcd_access" {
-  security_group_id = aws_security_group.control-plane-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 2379
-  to_port           = 2380
-  ip_protocol       = "tcp"
-}
+  ingress {
+    cidr_blocks = [aws_vpc.cluster-vpc.cidr_block]
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+  }
 
-resource "aws_vpc_security_group_egress_rule" "all_traffic_cp" {
-  security_group_id = aws_security_group.control-plane-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
 
+}
 
 resource "aws_security_group" "worker-sg" {
   name        = "${var.cluster_name}-worker-sg"
@@ -81,34 +77,29 @@ resource "aws_security_group" "worker-sg" {
   tags = {
     Name = "${var.cluster_name}-worker-sg"
   }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "kubelet_api" {
-  security_group_id = aws_security_group.worker-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 10250
-  to_port           = 10250
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ssh_w" {
-  security_group_id = aws_security_group.worker-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "node_port_services" {
-  security_group_id = aws_security_group.worker-sg.id
-  cidr_ipv4         = aws_vpc.cluster-vpc.cidr_block
-  from_port         = 30000
-  to_port           = 32767
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_egress_rule" "all_traffic_w" {
-  security_group_id = aws_security_group.worker-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  ingress {
+    cidr_blocks = [aws_instance.bastion.public_ip]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = [aws_vpc.cluster-vpc.cidr_block]
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+  }
+  ingress {
+    cidr_blocks = [aws_vpc.cluster-vpc.cidr_block]
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
 }
